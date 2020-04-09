b006f5b9781355a1a5764e8ef47f3bf0b4cb854d
qcy-hand <1024809717@qq.com>
2020-04-09 18:04:48 +0800

    function 1

--- chongwudaily/miniprogram/pages/yimiao/yimiao.js
+++ chongwudaily/miniprogram/pages/yimiao/yimiao.js
@@ -1,6 +1,6 @@
 // miniprogram/pages/yimiao/yimiao.js
 import Toast from '@vant/weapp/toast/toast';
-
+import Dialog from '@vant/weapp/dialog/dialog';
 
 Page({
 
@@ -8,23 +8,71 @@ Page({
    * 页面的初始数据
    */
   data: {
+    //选择
     xuan_popup: false,
-    checked: false,
-    fileList: [],
+    xuan_chong: "",
     chong_columns: ['张三', '李四', '王老五'],
-    zhu_popup:false,
+
+    // 注射
+    zhu_popup: false,
+    zhu_time: "",
     minHour: 10,
     maxHour: 20,
-    minDate: new Date().getTime(),
-    maxDate: new Date(2030, 10, 1).getTime(),
-    currentDate: new Date().getTime(),
-   
-    fan_popup:false,
-    fan_columns:['良好','一般','不适'],
+    zhu_minDate: new Date(2010, 10, 1).getTime(),
+    zhu_maxDate: new Date(2030, 10, 1).getTime(),
+    zhu_currentDate: new Date().getTime(),
+
+    //反应
+    fan_popup: false,
+    fan_columns: ['良好', '一般', '不适'],
+    fan_zhu: "",
+
+    //下次
     xia_popup: false,
-    pai_value:''
+    xia_time: "", //下次时间
+    xia_minDate: new Date().getTime(),
+    xia_maxDate: new Date(2030, 10, 1).getTime(),
+    xia_currentDate: new Date().getTime(),
+
+    //品牌
+    id_pai: "", //输入的品牌
+
+    //添加图片
+    fileList: [],
+
+    //提醒
+    checked: false,
+
+    //完成按钮禁用状态
+    yimiao_jin: true,
+    yimiao: false,
+
+    //Push
+    Timestamp: "", //时间戳 用于排序
   },
 
+  //判断所有内容是否填完整，以启用按钮
+  checking() {
+    let that = this;
+    if (that.data.xuan_chong !== "" && that.data.zhu_time !== "" && that.data.fan_zhu !== "" && that.data.xia_time !== "" && that.data.id_pai !== "") {
+      that.setData({
+        yimiao_jin: false,
+        yimiao: true
+      }, () => {
+        console.log('内容已填满')
+      });
+
+    } else {
+      that.setData({
+        yimiao_jin: true,
+        yimiao: false
+      }, () => {
+        console.log('内容未填满')
+      });
+    }
+  },
+
+  //选择宠物
   openchong() {
     this.setData({
       xuan_popup: true
@@ -36,18 +84,17 @@ Page({
     })
   },
   chong_confirm(event) {
-    const {
-      picker,
-      value,
-      index
-    } = event.detail;
-    Toast(`当前值：${value}, 当前索引：${index}`);
+
+    this.setData({
+      xuan_popup: false,
+      xuan_chong: event.detail.value,
+    }, () => {
+      console.log(event.detail.value)
+      this.checking()
+    });
   },
 
-  chong_cancel() {
-    Toast('取消');
-  },
-
+  //注射时间
   openzhu() {
     this.setData({
       zhu_popup: true
@@ -59,23 +106,32 @@ Page({
     })
   },
   zhu_confirm(event) {
-    const {
-      picker,
-      value,
-      index
-    } = event.detail;
-    Toast(`当前值：${value}, 当前索引：${index}`);
+    if (event.type == 'input') {
+      return
+    } else {
+      var a = event.detail
+      console.log(event)
+      let that = this
+
+      function getDate(a) {
+        var now = new Date(a),
+          y = now.getFullYear(),
+          m = now.getMonth() + 1,
+          d = now.getDate(),
+          h = now.getHours(),
+          n = now.getMinutes()
+        return y + "-" + (m < 10 ? "0" + m : m) + "-" + (d < 10 ? "0" + d : d) + " " + (h < 10 ? "0" + h : h) + ":" + (n < 10 ? "0" + n : n);
+      }
+      that.setData({
+        zhu_popup: false,
+        zhu_time: getDate(a)
+      }, () => {
+        that.checking()
+      });
+    }
   },
 
-  zhu_cancel() {
-    Toast('取消');
-  },
-  zhuInput(event) {
-    this.setData({
-      currentDate: event.detail
-    });
-  },
-
+  //注后反应
   openfan() {
     this.setData({
       fan_popup: true
@@ -87,18 +143,17 @@ Page({
     })
   },
   fan_confirm(event) {
-    const {
-      picker,
-      value,
-      index
-    } = event.detail;
-    Toast(`当前值：${value}, 当前索引：${index}`);
+
+    this.setData({
+      fan_popup: false,
+      fan_zhu: '反应' + event.detail.value,
+    }, () => {
+      this.checking()
+      console.log(event.detail.value)
+    });
   },
 
-  fan_cancel() {
-    Toast('取消');
-  },
-
+  //下次时间
   openxia() {
     this.setData({
       xia_popup: true
@@ -110,30 +165,41 @@ Page({
     })
   },
   xia_confirm(event) {
-    const {
-      picker,
-      value,
-      index
-    } = event.detail;
-    Toast(`当前值：${value}, 当前索引：${index}`);
+    if (event.type == 'input') {
+      return
+    } else {
+      var a = event.detail
+      let that = this
+
+      function getdate(a) {
+        var now = new Date(a),
+          y = now.getFullYear(),
+          m = now.getMonth() + 1,
+          d = now.getDate(),
+          h = now.getHours(),
+          n = now.getMinutes()
+        return y + "-" + (m < 10 ? "0" + m : m) + "-" + (d < 10 ? "0" + d : d) + " " + (h < 10 ? "0" + h : h) + ":" + (n < 10 ? "0" + n : n);
+      }
+      that.setData({
+        xia_popup: false,
+        xia_time: getdate(a)
+      }, () => {
+        that.checking()
+      });
+    }
   },
 
-  xia_cancel() {
-    Toast('取消');
-  },
-  xiaInput(event) {
+  // 疫苗品牌
+  Input_pai(event) {
     this.setData({
-      currentDate: event.detail
-    });
+      id_pai: event.detail,
+    }, () => {
+      this.checking()
+      console.log(event.detail)
+    })
   },
 
-  oChange(event) {
-    this.setData({
-      checked: event.detail
-    });
-  },
-
-
+  //添加图片
   afterRead(event) {
     const {
       file
@@ -159,6 +225,70 @@ Page({
           fileList
         });
       }
+    });
+  },
+
+  //提醒
+  oChange(event) {
+    this.setData({
+      checked: event.detail
+    });
+  },
+
+  //传值
+  Push() {
+    let that = this
+    //向数据库传数据
+    wx.cloud.callFunction({
+      name: 'yimiao_send',
+      data: {
+        type: 'yimiao',
+        xuan_chong: that.data.xuan_chong,
+        zhu_time: that.data.zhu_time,
+        fan_zhu: that.data.fan_zhu,
+        xia_time: that.data.xia_time,
+        id_pai: that.data.id_pai,
+        Timestamp: new Date().getTime(),
+      },
+      success(res) {
+        console.log(res);
+      },
+      fail() {
+        wx.showToast({
+          title: '系统错误，请稍后重试!',
+          duration: 1000,
+          icon: "none"
+        })
+      }
+    })
+  },
+
+
+  //回note页
+  to_note: function () {
+    wx.navigateTo({
+      url: '../yimiao_note/yimiao_note'
+    })
+  },
+
+  //完成——提交
+  push_miao() {
+    Dialog.confirm({
+      message: '填好啦？',
+      closeOnClickOverlay: true,
+      cancelButtonText: "再瞅瞅",
+      confirmButtonText: "嗯呐"
+    }).then(() => {
+      console.log('已点击确定');
+      this.Push(); //调用传值函数
+      this.to_note();//回note页
+      wx.showToast({
+        title: '成功',
+        icon: 'success',
+        duration: 2000,
+      });
+    }).catch(() => {
+      console.log('已点击取消');
     });
   },
 
--- chongwudaily/miniprogram/pages/yimiao_note/yimiao_note.js
+++ chongwudaily/miniprogram/pages/yimiao_note/yimiao_note.js
@@ -5,11 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    time: 30 * 60 * 60 * 1000, //倒计时
+    //倒计时
+    time: 30 * 60 * 60 * 1000,
     timeData: {},
-    
-    fileList: [
-      { url: 'https://img.yzcdn.cn/vant/leaf.jpg', name: '图片1' },
+
+    //预览图
+    fileList: [{
+        url: 'https://img.yzcdn.cn/vant/leaf.jpg',
+        name: '图片1'
+      },
       // Uploader 根据文件后缀来判断是否为图片文件
       // 如果图片 URL 中不包含类型信息，可以添加 isImage 标记来声明
       {
@@ -17,32 +21,63 @@ Page({
         name: '图片2',
         isImage: true
       },
-    ]
+    ],
+
+    //回调数组
+    wardyimiao:[]
   },
 
+  //倒计时
   onChange(e) {
     this.setData({
       timeData: e.detail
     });
   },
 
-  to_bianji:function(){
+  //去编辑
+  to_bianji: function () {
     wx.navigateTo({
       url: '../yimiao_bian/yimiao_bian'
     });
   },
 
-  to_yimiao:function(){
+  //去添加页
+  to_yimiao: function () {
     wx.navigateTo({
-      url:'../yimiao/yimiao'
+      url: '../yimiao/yimiao'
     })
   },
 
+  //取数据
+  Getinfo() {
+    let that = this;
+    wx.cloud.callFunction({
+      name: "yimiao_get",
+      success(res) {
+        that.setData({
+          wardyimiao: res.result.data,
+        }, () => {
+          wx.hideLoading();
+
+    console.log(res.result.data);
+
+        });
+      },
+      fail() {
+        wx.hideLoading();
+        wx.showToast({
+          title: '加载错误，请稍后重试!',
+          duration: 1000,
+          icon: "none"
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
@@ -56,7 +91,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    let that = this;
+    wx.showLoading({
+      title: "加载中...",
+    });
+    that.Getinfo();
+    console.log('show取到数据');
   },
 
   /**
